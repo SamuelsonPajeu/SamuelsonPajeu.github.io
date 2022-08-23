15536aed5169ef26186ba0c476735be51a292316
SamuelsonPajeu <samuelson.pajeu@gmail.com>
2022-08-23 09:02:08 -0300

    Http foxes Front Update

---
+++ README.md
@@ -0,0 +1,19 @@
+# Samuelson Pajeu
+
+
+# Http foxes
+## Contributors
+
+Thanks go to these wonderful people:
+
+<table>
+    <tr>
+        <td align="center"><a href="https://github.com/HenriqueGomez"><img src="https://avatars.githubusercontent.com/u/11247187?v=4" width="100px;" alt=""/><br /><sub><b>Henrique Gomez</b></sub></a>
+        <a href="#" title="All Js and CSS front-end">💻</a>
+        </td>
+
+        <td align="center"><a href="https://github.com/Felipejsr"><img src="https://avatars.githubusercontent.com/u/32332877?v=4" width="100px;" alt=""/><br /><sub><b>Felipe Rodrigues</b></sub></a>
+        <a href="#" title="Foxes: 101,203,204,205">🦊</a>
+        </td>
+    </tr>
+</table>
--- assets/js/httpfoxes.js
+++ assets/js/httpfoxes.js
@@ -11,7 +11,6 @@ getFoxes().then(data => {
     loadWebSite(foxes);
 });
 
-
 function loadWebSite(foxes) {
     const foxesContainer = document.getElementById('foxes-container');
     const loadingContainer = document.getElementById('loading-foxes');
@@ -19,10 +18,12 @@ function loadWebSite(foxes) {
         const foxElement = document.createElement('div');
         foxElement.className = 'fox';
         foxElement.innerHTML = `
-            <li>
-                <img src="${fox.url}" alt="${fox.description}" class="thumb-image" >
-                <h2>${fox.code}</h2>
-                <p>${fox.description}</p>
+            <li class="thumb-li">
+                <img src="${fox.url}" alt="${fox.description}" class="thumb-image" draggable="false">
+                <div class="infoContainer">
+                    <h2>${fox.code}</h2>
+                    <p>${fox.description}</p>
+                </div>
             </li>
         `;
         foxesContainer.appendChild(foxElement);
@@ -30,3 +31,31 @@ function loadWebSite(foxes) {
     loadingContainer.style.display = 'none';
 }
 
+function copyToClipboard(l, i) {
+    let last = Date.now();
+    let link = document.getElementById(l.toString()).innerText;
+
+    //Copy link to clipboard
+    navigator.clipboard.writeText(link);
+
+    //Remove current div with 'p_active' class
+    let tags = document.getElementsByClassName('p_active');
+    if (tags.length > 0) {
+        for (t = 0; t < tags.length; t++) {
+            tags[t].classList.remove('p_active');
+        }
+        document.getElementById(i).classList.add('p_active');
+    }
+
+    document.getElementById(i).classList.add('p_active');
+
+    //Remove class after 1500ms
+    setTimeout(() => {
+        document.getElementById(i).classList.remove('p_active');
+    }, 1500);
+}
+
+function toggleList() {
+    //Toggle 'tableActive' class to display the Return Values
+    document.getElementById('valueList').classList.toggle('tableActive');
+}
